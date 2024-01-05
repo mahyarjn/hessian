20117912bca1549ae2aa7dd09ad0691af50a4253
Mahyar JafariNodeh <136162130+mahyarjn80@users.noreply.github.com>
2024-01-04 20:30:40 -0500

    aa

--- hessian.py
+++ hessian.py
@@ -138,9 +138,9 @@ def main():
         # test_losses.append(test_loss)
 
         # accuracy = 100 * correct / total
-        # print(f"Epoch [{epoch + 1}/{num_epochs}], "
-        #       f"Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, "
-        #       f"Test Accuracy: {accuracy:.2f}%")
+        print(f"Epoch [{epoch + 1}/{num_epochs}], "
+              f"Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, ")
+        print(bulk[-1])
 
     bulkk = [f.cpu() for f in bulk]
     print(bulkk)
